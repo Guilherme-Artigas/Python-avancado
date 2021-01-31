447b5ee69a4ede5d2a0d5e27b8a29c2da12b3b34
Guilherme Artigas <guilherme.artigas92@gmail.com>
2021-01-31 19:13:56 -0300

    Python

--- Aula 21 – Funções (Parte 2)/Exercício 104 – Validando entrada de dados em Python/ex104_.py
+++ Aula 21 – Funções (Parte 2)/Exercício 104 – Validando entrada de dados em Python/ex104_.py
@@ -1,6 +1,2 @@
-"""Crie um programa que tenha a função leiaInt(), que vai funcionar de forma semelhante a função input() dp python, só
+"""Crie um programa que tenha a função leiaInt(), que vai funcionar de forma semelhante a função input() do python, só
 que fazendo a validação para aceitar somente um valor numérico. Ex: n = leiaInt('Digite um n')"""
-print('*-' * 10)
-while True:
-    i = int(input('Digite um número: '))
-    
